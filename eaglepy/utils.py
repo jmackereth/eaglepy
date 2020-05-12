7f6e3851444f7cfd54b944318c017f2efa793cf8
jmackereth <tedmackereth@gmail.com>
2020-05-12 16:15:51 +0100

    start utility module

---
+++ eaglepy/utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def angular_momentum(coordinates, velocities, mass):
+    """
+    Compute the angular momentum of some coordinates and velocities
+    """
+    vec = np.cross(coordinates,velocities*mass[:,np.newaxis])
+    tot = np.sum(vec, axis=0)
+    return tot/np.linalg.norm(tot)
+
+def _transform(self,vector):
+    """Build a transformation matrix"""
+    a = vector
+    b = np.matrix([0,0,1])
+    v = np.cross(a,b)
+    s = np.linalg.norm(v)
+    c = np.dot(a,b.T)
+    vx = np.matrix([[0,-v[0,2],v[0,1]],[v[0,2],0,-v[0,0]],[-v[0,1],v[0,0],0]])
+    transform = np.eye(3,3) + vx + (vx*vx)*(1/(1+c[0,0]))
+    return transform
+
+
+def center_and_align(coordinates,velocities,mass, center=None, transform=None, align_percentile=10):
+    """Center and align the particles in the region either with a supplied transformation matrix
+        or by computation of the mean angular momentum of some range of particles (defined by some percentile)
+        of the radii
+    """
+    tcoordinates = coordinates - np.array(self.center)
+    radii = np.linalg.norm(tcoordinates, axis=1)
+    inside = radii < np.percentile(radii, align_percentile)
+    bulkvel = np.median(velocities[inside], axis=0)
+    tvelocities = velocities  - np.array(bulkvel)
+    if use_transform:
+        t = trans
+    else:
+        t = self._transform(self.angular_momentum(tcoordinates, tvelocities,mass))
+    if verbose:
+        print('Transforming Coordinates...')
+    for i in range(len(tcoordinates)):
+        tcoordinates[i] = np.einsum('ij,aj->ai', t, tcoordinates[i])
+        tvelocities[i] = np.einsum('ij,aj->ai', t, tvelocities[i])
+    if return_transform:
+        return tcoordinates, tvelocities, t
+    else:
+        return tcoordinates, tvelocities
