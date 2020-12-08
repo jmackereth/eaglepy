05ab5c7507634c612c0ba27ac760160f6c3b87a5
jmackereth <tedmackereth@gmail.com>
2020-12-08 11:48:12 -0500

    include conversion to physical units in subfind

--- eaglepy/h5read.py
+++ eaglepy/h5read.py
@@ -488,30 +488,6 @@ class Subfind:
             out.append(np.array(h5py.File(file, 'r')[dataset])[:] * factor)
         return np.concatenate(out)
 
-    def get_dataset(self, parttype, dataset, physical=False, cgs=False):
-        """ get the data for a given entry in the HDF5 file for the given region """
-        if not self.ParticleTypePresent[parttype]:
-            warnings.warn('Particle type is not present, returning empty arrays...')
-            return np.array([])
-        key = os.path.join('/PartType'+str(parttype),dataset)
-        if physical:
-            #find conversion factor
-            factor = self._conversion_factor(key, self.a0, self.HubbleParam, cgs=cgs)
-        elif not physical and cgs:
-            factor = h5py.File(self.files[0], 'r')[key].attrs['CGSConversionFactor']
-        else:
-            #else just multiply by 1!
-            factor = 1.
-        out = []
-        ptypeind = self._ptypeind[parttype]
-        for ii,file in enumerate(self.files_for_region[ptypeind]):
-            if not _particle_type_present(parttype, file):
-                continue
-            # load this file and get the particles
-            out.append(np.array(h5py.File(file, 'r')[key])[self.indices[ptypeind][ii]] * factor)
-        if len(out) < 2:
-            return out[0]
-        return np.concatenate(out)
 
     def _conversion_factor(self, key, a, h, cgs=False):
         aexp_scale, h_scale = self._get_conversion_factor_exponents(key)
